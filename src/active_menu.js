bfac86a9c0527802da40e55ee40a63012e65394b
강동욱 <ehddnr8563@gmail.com>
2023-12-14 02:04:49 +0900

    add: 인터섹팅 옵저버 추가 및 수정

--- src/active_menu.js
+++ src/active_menu.js
@@ -5,24 +5,41 @@
 // 보여지는 섹션:
 // - 다수의 섹션이 동시에 보여진다면, 가장 첫번째 섹션을 선택
 // - 마지막 contact 섹션이 보여진다면, 그러면 가장 마지막 섹션을 선택
-const sectionIds = ['#home', '#about', '#skills', '#projects', '#opinions', '#contact'];
+const sectionIds = [
+  '#home',
+  '#about',
+  '#skills',
+  '#projects',
+  '#opinions',
+  '#contact',
+];
 const $sections = sectionIds.map((id) => document.querySelector(id));
-const $navMenus = sectionIds.map((id) => document.querySelector(`[href='${id}']`));
+const $navItems = sectionIds.map((id) =>
+  document.querySelector(`[href='${id}']`)
+);
 const visibleSections = sectionIds.map(() => false);
+let activeNavItem = $navItems[0];
 
-const options = {};
+const options = {
+  rootMargin: '-20% 0px 0px 0px',
+  threshold: [0, 0.99],
+};
 const observerCallback = (entries) => {
-  let selectLastOne;
+  let selectLastOne; // flag 변수(마지막 섹션 활성화 여부)
   entries.forEach((entry) => {
     const index = sectionIds.indexOf(`#${entry.target.id}`);
     visibleSections[index] = entry.isIntersecting;
-    selectLastOne = index === sectionIds.length - 1 && entry.isIntersecting && entry.intersectionRatio >= 0.99;
+    selectLastOne =
+      index === sectionIds.length - 1 &&
+      entry.isIntersecting &&
+      entry.intersectionRatio >= 0.95;
   });
-  console.log(visibleSections);
-  console.log('마지막섹션활성화', selectLastOne);
 
-  const navIndex = selectLastOne ? sectionIds.length - 1 : findFirstIntersecting(visibleSections);
-  console.log(sectionIds[navIndex]);
+  const navIndex = selectLastOne
+    ? sectionIds.length - 1
+    : findFirstIntersecting(visibleSections);
+
+  selectNavItem(navIndex);
 };
 
 const observer = new IntersectionObserver(observerCallback, options);
@@ -33,4 +50,12 @@ $sections.forEach((section) => {
 const findFirstIntersecting = (intersections) => {
   const index = intersections.indexOf(true);
   return index >= 0 ? index : 0; // indexOf는 -1을 리턴할 가능성이 있기 때문에 안전하게 코딩
+};
+
+const selectNavItem = (index) => {
+  const navItem = $navItems[index];
+  if (!navItem) return;
+  activeNavItem.classList.remove('active');
+  activeNavItem = navItem;
+  activeNavItem.classList.add('active');
 };
